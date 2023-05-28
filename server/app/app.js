37689924fab18a34bea580c378e66f5727286393
Krzysztof Radzięta <kustosz142@gmail.com>
2023-05-28 17:17:07 +0200

    feat(server-side): added cors and do test run

---
+++ server/app/app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
