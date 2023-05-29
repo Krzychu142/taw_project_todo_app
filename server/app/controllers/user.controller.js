e7a031fb4bd387378765ca8968ec696733c67bc2
Krzysztof Radzięta <kustosz142@gmail.com>
2023-05-29 20:19:19 +0200

    feat(server-side): feature login, to be tested

--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -43,4 +43,28 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid email or password." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid email or password." });
+    }
+
+    const token = generateJWT(user._id, user.name);
+
+    res.status(200).json({ message: "User logged in successfully.", token });
+  } catch (error) {
+    res.status(500).json({ error: "Error logging in user." });
+  }
+};
+
+module.exports = { registerUser, loginUser };
