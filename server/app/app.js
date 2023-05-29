4ab06f4e59642c4f656f594edefe2697d348752b
Krzysztof Radzięta <kustosz142@gmail.com>
2023-05-29 18:22:42 +0200

    feat(server-side): added singleton class to connect with db

--- server/app/app.js
+++ server/app/app.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const DatabaseConnection = require("./db/DatabaseConnection");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const connection = new DatabaseConnection();
+connection.createConnection();
 
 const port = process.env.PORT || 3001;
 
