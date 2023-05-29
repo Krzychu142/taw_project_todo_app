80ad8a0c939943872c007b54d83dac51989f38cf
Krzysztof Radzięta <kustosz142@gmail.com>
2023-05-29 18:44:34 +0200

    feat(server-side): added models for single todo and for the user

---
+++ server/app/db/models/signleTodo.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String },
+  completed: { type: Boolean, default: false },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = Todo;
