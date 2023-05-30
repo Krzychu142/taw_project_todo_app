a46cd1f793bea2d07acede3267031fa5af48c6e6
Krzysztof Radzięta <kustosz142@gmail.com>
2023-05-30 22:56:50 +0200

    feat(client-side): start of works on home component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -6,10 +7,30 @@ import { AuthService } from '../auth.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  constructor(private authService: AuthService) {}
+export class HomeComponent implements OnInit {
+  todos: any[] = [];
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   isUserAuthenticated(): boolean {
     return this.authService.userIsAuthenticated;
+  }
+
+  ngOnInit(): void {
+    this.fetchTodos();
+  }
+
+  fetchTodos(): void {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    this.http.get<any>('http://localhost:3001/todos/todos', { headers }).subscribe(
+      response => {
+        this.todos = response.todos;
+      },
+      error => {
+        console.error(error);
+      }
+    );
   }
 }
